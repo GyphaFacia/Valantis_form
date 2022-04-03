71bf05d0773564c6ec3048ba0ebe88846f856169
Blank <holoparrentattachment@gmail.com>
2022-04-04 01:09:06 +0300

    adding inputs according to selected value

--- script.js
+++ script.js
@@ -80,6 +80,28 @@ const options = {
     'Другое': {},
 }
 
+function createChild(ops){
+    const notAttributes = `className text tagName`.split(' ')
+    const node = document.createElement(ops.tagName ?? 'div')
+
+    if(ops.className){
+        node.className = ops.className
+    }
+    if(ops.text){
+        const textnode = document.createTextNode(ops.text)
+        node.appendChild(textnode)
+    }
+
+    const filtered = Object.keys(ops).filter(
+        key => !notAttributes.includes(key)
+    ).forEach(
+        key => node.setAttribute(key, ops[key])
+    )
+
+    this.appendChild(node)
+    return node
+}
+
 const grid = document.querySelector('.eval-content-images-uploaded')
 
 for (let i = 0; i < 9; i++) {
@@ -95,13 +117,11 @@ for (let i = 0; i < 9; i++) {
 
 // вычислить размер плейсхолдера у инпута
 function measurePlaceholderWidth(inputDom){
-    const node = document.createElement('span')
-    const textnode = document.createTextNode(
-        inputDom.getAttribute('placeholder')
-    )
-
-    node.appendChild(textnode)
-    document.body.appendChild(node)
+    document.body.createChild = createChild
+    const node = document.body.createChild({
+        tagName: 'span',
+        text: inputDom.getAttribute('placeholder'),
+    })
 
     const style = getComputedStyle(inputDom)
     node.style.font = style.font
@@ -171,9 +191,10 @@ function constructDropMenu(dropdown, options){
 // обработать смену выбора типа изделия, добавить соответствующие инпуты
 function handleDropDownMenuChange(dropdown, options){
     removeOldInputs()
-    // console.log(dropdown.value)
     const inputs = options[dropdown.value]
-    // console.log(inputs)
+    Object.keys(inputs).forEach(
+        key => createInput(key, inputs[key])
+    )
 }
 
 // убрать инпуты старого состояния меню выбора типа изделия
@@ -188,8 +209,50 @@ function removeOldInputs(){
     }
 }
 
+// добавить инпут указанного типа
+function createInput(inputName, inputSettings){
+    const wrapper = inputsSection.createChild({
+        tagName: 'div',
+        className: 'eval-content-inputs-top-input-wrapper',
+    })
+    wrapper.createChild = createChild
+
+    if(inputSettings.type == 'checkbox'){
+        const input = wrapper.createChild({
+            tagName: 'input',
+            className: 'form-checkbox',
+            type: inputSettings.type,
+            checked: inputSettings.default,
+            name: inputName,
+        })
+
+        const label = wrapper.createChild({
+            tagName: 'label',
+            text: inputName,
+            className: 'form-checkbox-label',
+            for: inputName,
+        })
+        wrapper.style.flexDirection = 'row'
+        wrapper.classList.add('form-checkbox-wrapper')
+
+        return input
+    }
+
+    const input = wrapper.createChild({
+        tagName: 'input',
+        className: 'form-input',
+        placeholder: inputName,
+        type: inputSettings.type,
+        value: inputSettings.default ?? ""
+    })
+    return input
+}
+
+
 const dropdown = document.querySelector('select.form-input')
+dropdown.createChild = createChild
 const inputsSection = document.querySelector('.eval-content-inputs-top')
+inputsSection.createChild = createChild
 addAsterisksToRequiredInputs()
 constructDropMenu(dropdown, options)
 
