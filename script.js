7f7b308b43a282492640a6490c1a7bcc1dd58ce1
Blank <holoparrentattachment@gmail.com>
2022-04-04 15:05:30 +0300

    fixed min max step filters

--- script.js
+++ script.js
@@ -17,15 +17,15 @@ const options = {
             type: 'number',
             validation: validateInt,
             step: 1,
-            min: 375,
-            max: 750,
+            min: 333,
+            // max: 750,
         },
         'Вес гр.': {
             required: true,
             type: 'number',
             validation: validateFloat,
             min: 0,
-            max: 1000000,
+            // max: 1000000,
         },
     },
     'Драгоценные камни': {
@@ -34,7 +34,7 @@ const options = {
             type: 'number',
             validation: validateFloat,
             min: 0,
-            max: 3106,
+            // max: 3106,
         },
         'Наличие документов':{
             type: 'checkbox',
@@ -60,69 +60,77 @@ const options = {
             required: true,
             type: 'number',
             validation: validateInt,
-            step: 64,
+            step: 1,
             min: 0,
-            max: 512,
+            // max: 512,
         }
     },
     'Антиквариат': {},
     'Другое': {},
 }
 
-async function checkEmail(mail){
-    try {
-        const API = 'https://api.testmail.top/domain/check?data='
-        const url = `${API}${mail}`
-        const resp = await fetch(url, {
-            method: 'GET',
-            headers: {
-                'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvdGVzdG1haWwudG9wIiwiYXVkIjoiaHR0cHM6XC9cL2FwaS50ZXN0bWFpbC50b3AiLCJ1c2VyIjoiNjI0YTQxMmI5ZGFiMiIsInN1YiI6ImFwaSIsImlhdCI6MTY0OTAzMzUzMiwiZXhwIjo0ODA0NzEwNzMyfQ.CNz8DdGV-63NHoI3uUB7jgI047JrF7LSO_ihkANlX4E'
-            }
-        })
-        const {result, message} = await resp.json()
-        return {result, message}
-    } catch (error) {
-        console.warn(error)
-        return {result: false, message: '...'}        
-    }
-}
 
-async function handleFormSubmit(e){
+// 10-minute-mail - осознанный выбор пользователя
+// async function checkEmail(mail){
+//     try {
+//         const API = 'https://api.testmail.top/domain/check?data='
+//         const url = `${API}${mail}`
+//         const resp = await fetch(url, {
+//             method: 'GET',
+//             headers: {
+//                 'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvdGVzdG1haWwudG9wIiwiYXVkIjoiaHR0cHM6XC9cL2FwaS50ZXN0bWFpbC50b3AiLCJ1c2VyIjoiNjI0YTQxMmI5ZGFiMiIsInN1YiI6ImFwaSIsImlhdCI6MTY0OTAzMzUzMiwiZXhwIjo0ODA0NzEwNzMyfQ.CNz8DdGV-63NHoI3uUB7jgI047JrF7LSO_ihkANlX4E'
+//             }
+//         })
+//         const {result, message} = await resp.json()
+//         return {result, message}
+//     } catch (error) {
+//         console.warn(error)
+//         return {result: false, message: '...'}        
+//     }
+// }
+
+function handleFormSubmit(e){
     let inputs = [...form.querySelectorAll('input')].map(inp => inp.value)
     let [name, phone, mail] = inputs
 
-    if(!name){
-        alert('Поле имени не может быть пустым')
-        return null
-    }
+    // if(!name){
+    //     alert('Поле имени не может быть пустым')
+    //     return null
+    // }
 
-    if(!phone && !mail){
-        alert('Введите Ваш номер телефона или адрес электронной почты')
-        return null
-    }
+    // if(!phone && !mail){
+    //     alert('Введите Ваш номер телефона или адрес электронной почты')
+    //     return null
+    // }
 
-    if(phone && validatePhone(phone)){
-        alert(validatePhone(phone))
-        return null
-    }
+    // if(phone && validatePhone(phone)){
+    //     alert(validatePhone(phone))
+    //     return null
+    // }
 
-    if(mail && validateMail(mail)){
-        alert(validateMail(mail))
-        return null 
-    }
+    // if(mail && validateMail(mail)){
+    //     alert(validateMail(mail))
+    //     return null 
+    // }
 
-    if(mail){
-        try {
-            const domain = mail.split('@').pop()
-            if(!(await checkEmail(domain)).result){
-                alert(`Нам не знаком ваш почтовый ящик "${domain}"`)
-                return null
-            }
-        } catch (error) {
-            console.warn(error)
-        }
-    }
+    // if(mail){
+    //     try {
+    //         const domain = mail.split('@').pop()
+    //         const resp = await checkEmail(domain)
+    //         if(!resp.result){
+    //             if()
+    //             alert(`Нам не знаком ваш почтовый ящик "${domain}"`)
+    //             return null
+    //         }
+    //     } catch (error) {
+    //         console.warn(error)
+    //     }
+    // }
     
+    const keys = Object.keys(options[dropdown.value]).map(
+        (key, i) => ({[key] : inputs[i + 3]})
+    )
+    console.log(keys)
 
     e.preventDefault()
     console.log('OK')
